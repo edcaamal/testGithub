78046b4f75b5c9bc4b246587c23057d3d1dbc751
Edgar David Caamal <edcaamal@uacam.mx>
2023-04-17 15:00:38 -0500

    Estetica del Archivo inicial

--- testGithub/src/testgithub/TestGithub.java
+++ testGithub/src/testgithub/TestGithub.java
@@ -1,23 +1,11 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package testgithub;
 
-/**
- *
- * @author davidcaamal
- */
 public class TestGithub {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
-        System.out.println("Test GitHub");
         System.out.println("Universidad Autonóma de Campeche");
         System.out.println("Facultad de Ingeniería");
+        System.out.println("Ingeniería en Sistemas Computacionales");
     }
     
 }
