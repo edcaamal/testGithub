22dfe8b21f752c9f4542f287b1834ff1d6a7a0d5
Edgar David Caamal <edcaamal@uacam.mx>
2023-04-18 14:43:09 -0500

    Crear Método de llenar municipio

--- testGithub/src/testgithub/TestGithub.java
+++ testGithub/src/testgithub/TestGithub.java
@@ -1,11 +1,22 @@
 package testgithub;
 
+import java.util.ArrayList;
 import models.Municipio;
 
 public class TestGithub {
-
+    static ArrayList<Municipio> municipios = new ArrayList<>();
+    
+    public static void llenarMunicipios(){
+        municipios.add(new Municipio(1, "Calinki"));
+        municipios.add(new Municipio(2, "Campeche"));
+        municipios.add(new Municipio(3, "Carmen"));
+        System.out.println(municipios);
+        
+    }
+    
     public static void main(String[] args) {
-
+        llenarMunicipios();
+/*
         Municipio calkini = new Municipio(1, "Calinki");
         System.out.println(calkini.toString());
         
@@ -13,6 +24,7 @@ public class TestGithub {
         campeche.setId(2);
         campeche.setNombre("Campeche");
         System.out.println(campeche.toString());
+*/
 
     }
     
