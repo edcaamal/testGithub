0f99372048892d17314b814d0839abe0bb4b8b4a
Edgar David Caamal <edcaamal@uacam.mx>
2023-04-18 15:18:09 -0500

    Metodos para insertar y eliminar

--- testGithub/src/testgithub/TestGithub.java
+++ testGithub/src/testgithub/TestGithub.java
@@ -7,24 +7,43 @@ public class TestGithub {
     static ArrayList<Municipio> municipios = new ArrayList<>();
     
     public static void llenarMunicipios(){
-        municipios.add(new Municipio(1, "Calinki"));
-        municipios.add(new Municipio(2, "Campeche"));
-        municipios.add(new Municipio(3, "Carmen"));
+        municipios.add(new Municipio(1, "Calinki")); //0
+        municipios.add(new Municipio(2, "Campeche")); //1
+        municipios.add(new Municipio(3, "Carmen"));   //2
+        municipios.add(new Municipio(7, "Palizada"));  //3
         System.out.println(municipios);
         
     }
     
+    public static void listarMunicipiosForE(){
+        System.out.println("For E");
+        for (Municipio municipio : municipios) {
+            System.out.println(municipio);
+        }
+    } 
+    
+    public static void listarMunicipiosForI(){
+        System.out.println("For I");
+        for (int i = 0; i < municipios.size(); i++) {
+            System.out.println(municipios.get(i).toString());
+        }
+    }
+    
+    public static void eliminarMunicipio(int id){
+        // TODO Validar que el registro exita
+        municipios.remove(id-1);
+    }
+    
+    public static void insertarMunicipio(int id, String nombre){
+        // TODO Validar que el registro no exista
+        municipios.add(new Municipio(id, nombre));
+    }
+    
     public static void main(String[] args) {
         llenarMunicipios();
-/*
-        Municipio calkini = new Municipio(1, "Calinki");
-        System.out.println(calkini.toString());
-        
-        Municipio campeche = new Municipio();
-        campeche.setId(2);
-        campeche.setNombre("Campeche");
-        System.out.println(campeche.toString());
-*/
+        listarMunicipiosForE();
+        eliminarMunicipio(3);
+        listarMunicipiosForI();
 
     }
     
